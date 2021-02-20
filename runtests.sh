6b13696e3815670d36f342f972990e208c8502ce
Dylan Tsuruda <tsuruda.dylan@yahoo.com>
2021-02-20 03:58:16 -1000

    Make test output more understandable

--- runtests.sh
+++ runtests.sh
@@ -9,10 +9,10 @@ for filename in ./tests/*; do
     g++ -std=c++11 -Wall -o test tests-main.o nest_msa.o "$filename"
 
     echo "${BOLD}=================================================================================================${NORMAL}"
-    echo "${BOLD}Testing $filename${NORMAL}"
+    echo "${BOLD}Testing $(basename "$filename" .cpp)()${NORMAL}"
     echo "${BOLD}-------------------------------------------------------------------------------------------------${NORMAL}"
     ./test
-    echo "${BOLD}End of $filename${NORMAL}"
+    echo "${BOLD}End of tests for $(basename "$filename" .cpp)()${NORMAL}"
     echo "${BOLD}=================================================================================================${NORMAL}"
     echo
     echo
