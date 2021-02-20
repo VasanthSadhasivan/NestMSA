a8210ac744f85ee6f686793c31ed0b89762963e7
Dylan Tsuruda <tsuruda.dylan@yahoo.com>
2021-02-20 01:50:39 -1000

    Add function declarations in header file

--- nest_msa.h
+++ nest_msa.h
@@ -24,4 +24,42 @@ struct Particle
     double best_value;
 };
 
+struct MostFrequent
+{
+    int freq;
+    char freqChar;
+};
+
+Matrix create_peer_matrix(int strArrayLen, char **strArray);
+
+double weight(char *row, double w1=0.25, double w2=0.5, double w3=1.0);
+
+double objective(Matrix M, int row_index, int end_index=-1);
+
+bool full_row(char *row);
+
+Matrix remove_missing_rows(Matrix M);
+
+Particle getposition(int value, int rowindex, Matrix M);
+
+MostFrequent mostfrequent(char *row);
+
+Matrix fly_down(Particle p, Matrix M, int stride=1);
+
+char *column(Matrix M, int i);
+
+bool aligned(char *row);
+
+Particle *create_swarm(int index, Matrix M);
+
+bool criteria3(Particle p, int new_index, Matrix M);
+
+bool criteria2(Particle p, int threshold);
+
+bool stopcriteria(Particle p, int newindex, Matrix M, int threshold=5, bool debug=false);
+
+Matrix skip_missing(Matrix M);
+
+Particle row_alignment(int index, Matrix M);
+
 #endif
